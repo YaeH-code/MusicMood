d7330562d4ec234ec1fd8e38dee497a5e798119d
zeronze <johan.mahe92@gmail.com>
2023-06-06 09:42:58 +0200

    heyheyhey
    heyheyhey

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/profile')]
 class ProfileController extends AbstractController
@@ -26,13 +27,16 @@ class ProfileController extends AbstractController
     }
 
     #[Route('//edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, UserRepository $userRepository): Response
+    public function edit(Request $request, UserRepository $userRepository , UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = $this->getUser();
         $form = $this->createForm(User1Type::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('plainPassword')->getData();
+            $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+            $user->setPassword($hashedPassword);
             $userRepository->save($user, true);
 
             return $this->redirectToRoute('app_profile_show', [], Response::HTTP_SEE_OTHER);
