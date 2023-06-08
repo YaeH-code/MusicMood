d438ba61e26bc94498ed1e5bf27e495391b44ad3
YaCyr <Yae.Prog22@outlook.com>
2023-06-08 16:28:31 +0200

    form comment

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,25 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content')
-       
-            ->add('music')
-            ->add('user')
+            ->add('user', TextType::class, [
+                'label' => 'Nom',
+                'attr'=>[
+                    'class' => 'form-control'
+                    ]
+                ])
+            ->add('music', TextType::class, [
+                'label' => 'Titre',
+                'attr'=>[
+                    'class' => 'form-control'
+                    ]
+                ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu',
+                'attr'=>[
+                    'class' => 'form-control',
+                    'style' => 'height: 100px'
+                    ]
+                ])
         ;
     }
 
